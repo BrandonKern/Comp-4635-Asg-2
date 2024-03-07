b59fcc784506364db50362c66358e4934e1a0a77
Nimrat <100167954+NB00001100@users.noreply.github.com>
2024-03-06 20:47:46 -0700

    Create ServerRPC.java

---
+++ UDP and TCP Connections/Server/ServerRPC.java
@@ -0,0 +1,33 @@
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server2 {
+    public  static void  main (String [] args) {
+        try {
+            final int serverPort = 7777;
+            CrissCrossPuzzleServer connection = new CrissCrossPuzzleServerImpl();
+            CrissCrossPuzzleServer exportObject = (CrissCrossPuzzleServer) UnicastRemoteObject.exportObject(connection, 0);
+            try {
+                LocateRegistry.getRegistry(serverPort).list();
+            } catch (RemoteException e) {
+                LocateRegistry.createRegistry(serverPort);
+            }
+
+            String URL = "rmi://" + InetAddress.getLocalHost().getHostAddress() + ":7777/CrissCrossPuzzleServer";
+            Naming.rebind(URL,exportObject);
+            System.out.println("waiting for the client");
+        }catch (RemoteException | UnknownHostException | MalformedURLException e)
+        {
+            e.printStackTrace();
+            System.out.println("Error while trying to connect to client object");
+
+        }
+
+
+    }
+}
