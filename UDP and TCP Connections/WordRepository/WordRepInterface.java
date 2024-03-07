64361f7f91123e27222ece29a9b57e219b1e34e2
RutuKaravadra <94875133+RutuKaravadra@users.noreply.github.com>
2024-03-07 12:43:58 -0700

    Create WordRepInterface.java

---
+++ UDP and TCP Connections/WordRepository/WordRepInterface.java
@@ -0,0 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface WordRepository extends Remote {
+    boolean createWord(String word) throws RemoteException;
+    boolean removeWord(String word) throws RemoteException;
+    boolean checkWord(String word) throws RemoteException;
+    String getRandomWord(int length) throws RemoteException;
+}
