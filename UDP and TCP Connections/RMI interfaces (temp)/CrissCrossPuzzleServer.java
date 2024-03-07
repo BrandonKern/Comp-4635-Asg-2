0fc9e65fd99495ab9d634d9bf0ff4806ac29d5d8
Nimrat <100167954+NB00001100@users.noreply.github.com>
2024-03-06 20:46:53 -0700

    Update CrissCrossPuzzleServer.java

--- UDP and TCP Connections/RMI interfaces (temp)/CrissCrossPuzzleServer.java
+++ UDP and TCP Connections/RMI interfaces (temp)/CrissCrossPuzzleServer.java
@@ -2,15 +2,12 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface CrissCrossPuzzleServer extends Remote {
-    public String startGame(int userId, int number_of_words, int failed_attemps_factor) throws RemoteException;
-    
-    public String guessLetter(int userId, char letter) throws RemoteException;
-    
-    public String guessWord(int userId, String word) throws RemoteException;
-    
-    public String restartGame(int userId) throws RemoteException;
-    
-    public String endGame(int userId) throws RemoteException;
-    
-    public void displayGame(int userId) throws RemoteException;
-}  
+    int login(int login_id) throws RemoteException;
+    void addWord(String word) throws RemoteException;
+    void removeWord(String word) throws RemoteException;
+    boolean checkWord(String word) throws RemoteException;
+    int checkScore(int user_id) throws RemoteException;
+    String startGame(int difficulty, int failed_attempts) throws RemoteException;
+    String guessLetter(int user_id, char letter) throws RemoteException;
+    String guessWord(int user_id, String word) throws RemoteException;
+}
