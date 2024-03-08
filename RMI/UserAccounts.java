54c1ab1f8ac310540fbcf73a2e6a0ab3ffc88674
RutuKaravadra <94875133+RutuKaravadra@users.noreply.github.com>
2024-03-08 10:16:56 -0700

    Update UserAccounts.java

--- RMI/UserAccounts.java
+++ RMI/UserAccounts.java
@@ -2,7 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface UserAccounts extends Remote {
-    String checkUser(String user_id) throws RemoteException;
+    Boolean checkUser(String user_id) throws RemoteException;
+    Boolean setUserInactive(String user_id) throws RemoteException;
     String checkUserScore(String user_id) throws RemoteException;
     String updateUserScore(String user_id) throws RemoteException;
 }
